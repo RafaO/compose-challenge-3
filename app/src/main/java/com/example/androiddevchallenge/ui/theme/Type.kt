cdca8cea0135bbff8ca3c3fd052e63919f54d864
Rafael Ortega <rafao1989@hotmail.com>
2021-03-15 19:17:20 +0100

    add text styles

--- app/src/main/java/com/example/androiddevchallenge/ui/theme/Type.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/theme/Type.kt
@@ -17,27 +17,61 @@ package com.example.androiddevchallenge.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.androiddevchallenge.R
 
 // Set of Material typography styles to start with
+
+const val CAPS = "smcp"
+
+val kulimParkFont = FontFamily(
+    Font(R.font.kulimpark_regular),
+    Font(R.font.kulimpark_light, FontWeight.Light)
+)
+
+val latoFont = FontFamily(
+    Font(R.font.lato_regular),
+    Font(R.font.lato_bold, FontWeight.Bold)
+)
+
 val typography = Typography(
+    h1 = TextStyle(
+        fontFamily = kulimParkFont,
+        fontSize = 28.sp,
+        letterSpacing = 1.15.sp,
+        fontWeight = FontWeight.Light,
+    ),
+    h2 = TextStyle(
+        fontFamily = kulimParkFont,
+        fontSize = 15.sp,
+        letterSpacing = 1.15.sp,
+        fontFeatureSettings = CAPS,
+    ),
+    h3 = TextStyle(
+        fontFamily = latoFont,
+        fontSize = 14.sp,
+        letterSpacing = 0.sp,
+        fontWeight = FontWeight.Bold,
+    ),
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-        /* Other default text styles to override
+        fontFamily = latoFont,
+        fontSize = 14.sp,
+        letterSpacing = 0.sp,
+    ),
     button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+        fontFamily = latoFont,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        letterSpacing = 1.15.sp,
+        fontFeatureSettings = CAPS
     ),
     caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+        fontFamily = kulimParkFont,
+        fontSize = 12.sp,
+        letterSpacing = 1.15.sp,
+        fontFeatureSettings = CAPS
     )
-    */
 )
