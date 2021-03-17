77ab43fd4661416afbc5801a2c885925e247bddd
Rafael Ortega <rafao1989@hotmail.com>
2021-03-17 19:23:55 +0100

    create circle item

---
+++ app/src/main/java/com/example/androiddevchallenge/ui/elements/CircleItem.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.androiddevchallenge.ui.elements
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import com.example.androiddevchallenge.model.Collection
+import dev.chrisbanes.accompanist.glide.GlideImage
+
+@Composable
+fun CircleItem(collection: Collection) =
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        GlideImage(
+            data = collection.imageUrl,
+            contentDescription = "image for ${collection.name}",
+            modifier = Modifier
+                .size(88.dp)
+                .clip(CircleShape),
+            contentScale = ContentScale.Crop
+        )
+
+        Text(collection.name, modifier = Modifier.paddingFromBaseline(24.dp))
+    }
